42cad1dbb8d691ff01e56b25c4fff2962286f326
co18355 <42859352+co18355@users.noreply.github.com>
2018-08-31 12:06:29 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,201 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+	int ans;
+	printf("\t\t\tWelcome to KBC 2018");
+	printf("\n\n Q1) Whch smartphone tools is used for sharing an internet connection? \n");
+	printf(" 1.GPS \t 2.Hotspot \t 3.Talkback \t 4.Microphone Jack \n");
+	scanf("%d",&ans);
+	if(ans==2)
+	{
+		printf("You won Rs. 5000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 2.Hotspot \n Final money won Rs. 0 \n Game over \n");
+		exit(0);
+	}
+	printf("\n Q2) Who was the lead actress in the film Padmavat? \n");
+	printf(" 1.Rani Mukherji \t 2.Anushka Sharma \t 3.Deepika Padukone \t 4.Katrina Kaif \n");
+	scanf("%d",&ans);
+	if(ans==3)
+	{
+		printf("You won Rs. 10,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.Deepika Padukone \n Final money won Rs. 5,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q3) Which game P V Sindhu plays? \n");
+	printf(" 1.Table Tennis \t 2.Lawn Tennis \t 3.Badminton \t 4.Squash \n");
+	scanf("%d",&ans);
+	if(ans==3)
+	{
+		printf("You won Rs. 20,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.Badminton \n Final money won Rs. 10,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q4) The South-West coast(Goa part of india) of India is popularly Known as? \n");
+	printf(" 1.Coromandel \t 2.Malabar \t 3.Amritsar \t 4.Konkan \n");
+	scanf("%d",&ans);
+	if(ans==4)
+	{
+		printf("You won Rs.40,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 4.Konkan \n Final money won Rs. 20,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q5) Which one these politians became the president of his party in december 2017? \n");
+	printf(" 1.K Chandrashekhar Rao \t 2.Raj Thackeray \t 3.Rahul Gandhi \t 4.Naveen Patnaik\n");
+	scanf("%d",&ans);
+	if(ans==3)
+	{
+		printf("You won Rs.80,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.Rahul Gandhi \n Final money won Rs. 40,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q6) In which of these region of Gujarat would you find towns named Gondal and Virpur Jalaram Temple? \n");
+	printf(" 1.Gohilwad \t 2.Garhwal \t 3.Saurashtra \t 4.Kumaon\n");
+	scanf("%d",&ans);
+	if(ans==3)
+	{
+		printf("You won Rs.1,60,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.Saurashtra \n Final money won Rs. 80,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q6) Recently which car was introduced by Tata motors during auto expo 2018 in India? \n");
+	printf(" 1.H5X and 45X \t 2.Tigor \t 3.Tiago \t 4.Ertiga\n");
+	scanf("%d",&ans);
+	if(ans==1)
+	{
+		printf("You won RS.3,20,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.H5X and 45X \n Final money won Rs. 1,60,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q7) Who among these is regarded as the inventor of the air conditioners? \n");
+	printf(" 1.Henry Ford \t 2.Willis Carrier \t 3.Issac Singer \t 4.Henning Holck-Larsen\n");
+	scanf("%d",&ans);
+	if(ans==2)
+	{
+		printf("You won Rs.6,40,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 2.Willis Carrier \n Final money won Rs. 3,20,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q8) Whos is the Chief Minister of Delhi in the year 2015? \n");
+	printf(" 1.Arvind Kejriwal \t 2.Devendra Fadnavis \t 3.Manohar Parrikar \t 4.Siddaramaiah\n");
+	scanf("%d",&ans);
+	if(ans==1)
+	{
+		printf("You won Rs.12,50,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Arvind Kejriwal \n Final money won Rs. 6,40,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q9) Which theoritical physicist passed away recently? \n");
+	printf(" 1.Issac Newton \t 2.Dailhousi \t 3.Jr Claudia \t 4.Stephen Hawking\n");
+	scanf("%d",&ans);
+	if(ans==4)
+	{
+		printf("You won Rs.25,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 2.Hotspot \n Final money won Rs. 5000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q10) Sardar Sarovar is built on which river? \n");
+	printf(" 1.Narmada \t 2.Tapi \t 3.Mahi \t 4.Chambal\n");
+	scanf("%d",&ans);
+	if(ans==1)
+	{
+		printf("You won Rs.50,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Narmada \n Final money won Rs. 25,00,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q11) Which 2017 hindi movie was planned to go at Oscar 2018? \n");
+	printf(" 1.I \t 2.Toilet \t 3.Newton \t 4.Badri ki Dulhaniya\n");
+	scanf("%d",&ans);
+	if(ans==3)
+	{
+		printf("You won Rs.1,00,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.Newton \n Final money won Rs. 50,00,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q12) Marwar is part of which state? \n");
+	printf(" 1.Punjab \t 2.Madhya Pradesh \t 3.Rajasthan \t 4.Utter Pradesh\n");
+	scanf("%d",&ans);
+	if(ans==3)
+	{
+		printf("You won Rs.2,00,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.Rajasthan \n Final money won Rs. 1,00,00,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q13) Which one these is an i=Indian government institution that controls all the communication and connectivity? \n");
+	printf(" 1.NIA \t 2.RBI \t 3.TRAI \t 4.NITI Aayog\n");
+	scanf("%d",&ans);
+	if(ans==3.)
+	{
+		printf("You won Rs.3,00,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.TRAI \n Final money won Rs. 2,00,00,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q14) Which of thes states that the maker of a product is responsible for repairing or replacing it if necessary? \n");
+	printf(" 1.Memory card \t 2.Warranty Card \t 3.Greeting Card \t 4.SIM Card\n");
+	scanf("%d",&ans);
+	if(ans==2)
+	{
+		printf("You won Rs.5,00,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 2.Warranty Card \n Final money won Rs. 3,00,00,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q15) Who is the current Foriegn Minister of India? \n");
+	printf(" 1.Sushma Swaraj \t 2.Mamata Banerjee \t 3.Smiriti Irani \t 4.Mayawat\n");
+	scanf("%d",&ans);
+	if(ans==1)
+	{
+		printf("You won Rs.7,00,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Sushma Swaraj \n Final money won Rs. 5,00,00,000 \n Game over\n");
+		exit(0);
+	}
+	return 0;
+}
